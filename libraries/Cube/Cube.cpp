2589d51a7b464684f88c2fcae2d3642680f4cea9
Mark Gadsby <Mark.Gadsby@Thermo.com>
2018-10-24 14:59:18 +0100

    Added column lighting functions

--- libraries/Cube/Cube.cpp
+++ libraries/Cube/Cube.cpp
@@ -18,39 +18,71 @@ CCube::~CCube()
 {
 }
 	
-void CCube::AllOff()
+////////////////////////////////////////////////////////////////////////////////
+//	Function name	: CCube::AllLow
+//	Description		: Will set all pins to LOW which is an All Off state.
+//
+//						Switching the layers HIGH from this state will turn each
+//						layer on.
+//
+////////////////////////////////////////////////////////////////////////////////
+void CCube::AllLow()
 {
-	int i = NUMCOLS;
+	int i = NUMLAYS;
 
-	// all columns (negatives) low
-	while (i--)
-		digitalWrite(m_Columns[i], LOW);
-  
-	i = NUMLAYS;
-  
-	// all columns (positives) low also.
+	// all layers (positives) low.
 	while (i--)
 		digitalWrite(m_Layers[i], LOW);
+
+	i = NUMCOLS;
+
+	// all columns (negatives) low also.
+	while (i--)
+		digitalWrite(m_Columns[i], LOW);
 }
+
+////////////////////////////////////////////////////////////////////////////////
+//	Function name	: CCube::AllHigh
+//	Description		: Will set all pins to HIGH which is an All Off state.
+//
+//						Switching the columns LOW from this state will turn 
+//						individual columns on.
+//
+////////////////////////////////////////////////////////////////////////////////
+void CCube::AllHigh()
+{
+	int i = NUMLAYS;
+
+	// all layers (positives) high.
+	while (i--)
+		digitalWrite(m_Layers[i], HIGH);
+
+	i = NUMCOLS;
+
+	// all columns (negatives) high also
+	while (i--)
+		digitalWrite(m_Columns[i], HIGH);
+}
+
 
 void CCube::AllOn()
 {
-	int i = NUMCOLS;
+	int i = NUMLAYS;
+
+	// all layers (positives) high
+	while (i--)
+		digitalWrite(m_Layers[i], HIGH);
+
+	i = NUMCOLS;
 
 	// all columns (negatives) low
 	while (i--)
 		digitalWrite(m_Columns[i], LOW);
-  
-	i = NUMLAYS;
-  
-	// all columns (positives) high
-	while (i--)
-		digitalWrite(m_Layers[i], HIGH);
 }		
 
 void CCube::LightLayer(int nLayer)
 {
-  AllOff();
+  AllLow();
 
   if (nLayer / 4 == 0)
   {
@@ -86,4 +118,90 @@ void CCube::LayerSweep()
   }
 }
 
+void CCube::SnakeForwardBackward()
+{
+	AllHigh();			// all off
+	SnakeForward();		// all on
+	SnakeBackward();	// all off
+}
 
+void CCube::EachColumnsLine()
+{
+	int y = 0;
+
+	while (y <= 3) // y == 0,1,2 & 3 
+	{
+		int x = 0;
+		while (x <= 3) // x == 0,1,2 & 3 
+		{
+			// Set individual column low
+			digitalWrite(m_Columns[x + (y * NUM_X_Y)], LOW);
+			delay(150);
+			x++;
+		}
+		y++;
+	}
+}
+
+void CCube::SnakeForward()
+{
+	int x = 0;
+	int y = 0;
+
+	while (y <= 3) // y == 0,1,2 & 3 
+	{
+		// So the x axis does the 'snaking'
+		if (x == 0)
+		{
+			while (x <= 3) // x == 0,1,2 & 3 
+			{
+				// Set individual column low
+				digitalWrite(m_Columns[x + (y * NUM_X_Y)], LOW);
+				delay(150);
+				x++;
+			}
+		}
+		else if (x == 3)
+		{
+			while (x >= 0) // x == 3,2,1 & 0 
+			{
+				// Set individual column low
+				digitalWrite(m_Columns[x + (y * NUM_X_Y)], LOW);
+				delay(150);
+				x--;
+			}
+		}
+		y++;
+	}
+}
+
+void CCube::SnakeBackward()
+{
+	int x = 3;
+	int y = 3;
+
+	while (y >= 0) // y == 3,2,1 & 0 
+	{
+		if (x == 3)
+		{
+			while (x >= 0) // x == 3,2,1 & 0 
+			{
+				// Set individual column high
+				digitalWrite(m_Columns[x + (y * NUM_X_Y)], HIGH);
+				delay(150);
+				x--;
+			}
+		}
+		else if (x == 0)
+		{
+			while (x <= 3) // x == 0,1,2 & 3 
+			{
+				// Set individual column low
+				digitalWrite(m_Columns[x + (y * NUM_X_Y)], HIGH);
+				delay(150);
+				x++;
+			}
+		}
+		y--;
+	}
+}
--- libraries/Cube/Cube.h
+++ libraries/Cube/Cube.h
@@ -5,6 +5,7 @@
 
 const int NUMCOLS = 16;
 const int NUMLAYS = 4;
+const int NUM_X_Y = 4;
 
 class CCube
 {
@@ -13,11 +14,17 @@ class CCube
 		virtual ~CCube();
 		
 		void AllOn();
-		void AllOff();
-		void LightLayer(int nLayer);
+		void AllLow();
+		void AllHigh();
 		void LayerSweep();
-		
+		void SnakeForwardBackward();
+		void EachColumnsLine();
+
 	private:
+		void LightLayer(int nLayer);
+		void SnakeForward();
+		void SnakeBackward();
+
 		// Column array
 		int m_Columns[NUMCOLS] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, A0, A1};
 
